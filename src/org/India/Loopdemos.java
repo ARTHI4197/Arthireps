8a30ec94fbb847a3035088a9819e78ca23951526
ARTHI4197 <arthi4197@gmail.com>
2022-10-02 23:14:29 +0530

    First Time Push

---
+++ src/org/India/Loopdemos.java
@@ -0,0 +1,18 @@
+package org.India;
+
+public class Loopdemos {
+
+	public static void main(String[] args) {
+		{int i=23;
+		
+		if(i<2) 
+		System.out.println("false");
+		
+		if (i>21)
+		System.out.println("true");}
+		
+		
+
+	}
+
+}
